11f0b41a10779aaaa9850b105ba6e767d0fe5cf8
Amanbel <amanuel11belachew@gmail>
2024-04-25 00:23:39 +0300

    5-sum

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,7 +7,4 @@ type FloatList = list[float]
 def sum_list(input_list: FloatList) -> float:
     """ function that returns the sum
     of the lists """
-    sum: float = 0
-    for i in range(0, len(input_list) - 1):
-        sum += input_list[i]
-    return sum
+    return sum(input_list)
