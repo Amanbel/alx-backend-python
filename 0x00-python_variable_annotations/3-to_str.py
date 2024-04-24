e8cb6abcc07e96545c294a386835ae19ad00cf3b
Amanbel <amanuel11belachew@gmail>
2024-04-24 23:58:48 +0300

    3-done

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" returning a string from a float """
+
+
+def to_str(n: float) -> str:
+    """ function that returns a string from
+    a float argument """
+    return "{}".format(n)
