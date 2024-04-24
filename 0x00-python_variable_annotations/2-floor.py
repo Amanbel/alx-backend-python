46b537a32ad8378d0f8495bc67190ac74f767155
Amanbel <amanuel11belachew@gmail>
2024-04-24 23:55:39 +0300

    done 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-import math
 """ return the floor of a number """
 
 
 def floor(n: float) -> int:
     """ function that returns the floor
     of the provided argument """
-    return math.floor(n)
+    return int(n)
