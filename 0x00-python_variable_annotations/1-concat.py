b2ed437b8319577c00cd5e3a64bef8a72e08826b
Amanbel <amanuel11belachew@gmail>
2024-04-24 21:20:13 +0300

    concat push

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" python string concatination with
+type annotation """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ function to concatinate strings """
+    return str1 + str2
