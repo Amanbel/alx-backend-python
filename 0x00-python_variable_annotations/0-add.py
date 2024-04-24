fa3b9ee9a1c32e03d4a5ea86c55b1d6fc3b55eeb
Amanbel <amanuel11belachew@gmail>
2024-04-24 15:09:13 +0300

    0 fix

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,6 @@
+""" python add function with type annotation """
+
 def add(a: float, b: float) -> float:
+    """ add function that accepts two arguments
+    both of which are floats """
     return a + b
