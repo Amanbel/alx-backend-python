dab144b2cd0c1dbcd715f475a35d67fb5eff3da4
Amanbel <amanuel11belachew@gmail>
2024-04-25 01:22:02 +0300

    101-done

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Complex types - generic types
+"""
+from typing import Mapping, TypeVar, Union, Any
+
+T = TypeVar['T']
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    annotated predefined function
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
