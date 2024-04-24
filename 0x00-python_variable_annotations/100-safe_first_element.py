f9c2565f90751b8beb2b3cfa0ee693f96ff3d53f
Amanbel <amanuel11belachew@gmail>
2024-04-25 01:07:41 +0300

    9-fix

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+complex types - annotating function
+"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    annotated predefined function
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
