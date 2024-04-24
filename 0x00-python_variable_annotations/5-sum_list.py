3a48799db310e298f3b8bbaeabe5eea8939dbf61
Amanbel <amanuel11belachew@gmail>
2024-04-25 00:25:30 +0300

    5-sum

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """ summing up floats """
+from typing import List
 
-type FloatList = list[float]
 
-
-def sum_list(input_list: FloatList) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ function that returns the sum
     of the lists """
     return sum(input_list)
