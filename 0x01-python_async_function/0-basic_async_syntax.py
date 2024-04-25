dc21ac92070fadb83ea32ff000346d0156d78fdb
Amanbel <amanuel11belachew@gmail>
2024-04-26 00:10:26 +0300

    0 issue done

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,13 +2,15 @@
 """
 async corutines
 """
+import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     function that returns a random
     num between 0 and max_delay
     """
     rand = await random.uniform(0, max_delay)
+    asyncio.sleep(rand)
     return rand
